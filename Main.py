03148e978f9fc775ff09e95befbc2e6a64a58130
Hedvaldo Costa Vieira de Oliveira <hedvaldocostaV.O@hotmail.com>
2023-05-11 14:38:58 -0300

    Vídeo e imagem

--- Main.py
+++ Main.py
@@ -1,6 +1,16 @@
 import streamlit as st
 import cv2
 from mtcnn import MTCNN
+import numpy as np
+
+
+def detect_faces(image):
+    detector = MTCNN()
+    result = detector.detect_faces(image)
+    for face in result:
+        x, y, width, height = face['box']
+        cv2.rectangle(image, (x, y), (x+width, y+height), (0, 255, 0), 2)
+    return image
 
 
 def detect_faces_video(video):
@@ -19,30 +29,31 @@ def detect_faces_video(video):
 
 def main():
     st.title("Detector de faces em vídeo com MTCNN")
-    button_videos = st.button('Testar com vídeos')
-    button_img = st.button('Testar com imagens')
     # Carrega o vídeo
-    if button_videos:
-        video_choice = {'video1': 'https://bit.ly/3pB4mmU',
-                        'video2': 'https://bit.ly/3LZSIZY',
-                        'video3': 'https://bit.ly/453LTzA',
-                        'video4': 'https://bit.ly/42MTkch'}
-        select_video = st.sidebar.selectbox('Selecione um vídeo', video_choice.keys())
-        video = cv2.VideoCapture(video_choice[select_video])
-        video_generator = detect_faces_video(video)
+    video_choice = {'': '',
+                    'video1': 'https://bit.ly/3pB4mmU',
+                    'video2': 'https://bit.ly/3LZSIZY',
+                    'video3': 'https://bit.ly/453LTzA',
+                    'video4': 'https://bit.ly/42MTkch'}
+    select_video = st.sidebar.selectbox('Selecione um vídeo', video_choice.keys())
+    video = cv2.VideoCapture(video_choice[select_video])
+    video_generator = detect_faces_video(video)
 
-        # Exibe o vídeo com as faces detectadas em tempo real
-        stframe = st.empty()
-        while True:
-            try:
-                frame = next(video_generator)
-                stframe.image(frame, channels="RGB")
-            except StopIteration:
-                break
-            if button_img:
-                break
-    elif button_img:
-        st.info('É O MENININ DE PAPAI É?')
+    # Exibe o vídeo com as faces detectadas em tempo real
+    stframe = st.empty()
+    while True:
+        try:
+            frame = next(video_generator)
+            stframe.image(frame, channels="RGB")
+        except StopIteration:
+            break
+
+    uploaded_file = st.sidebar.file_uploader("Carregue uma imagem", type=["jpg", "jpeg", "png"])
+    if uploaded_file is not None:
+        # decodificar uma imagem codificada em um formato específico em uma imagem OpenCV
+        image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
+        image = detect_faces(image)
+        st.image(image, channels="BGR")
 
 
 if __name__=="__main__":
