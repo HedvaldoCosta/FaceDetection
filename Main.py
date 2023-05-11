21cbe092c4c0b5ccdad3bbc7df28eb87b2b6191c
Hedvaldo Costa Vieira de Oliveira <hedvaldocostaV.O@hotmail.com>
2023-05-11 14:12:50 -0300

    Testando Vídeos

--- Main.py
+++ Main.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import cv2
-import numpy as np
 from mtcnn import MTCNN
-import io
 
 
 def detect_faces_video(video):
@@ -23,19 +21,22 @@ def main():
     st.title("Detector de faces em vídeo com MTCNN")
 
     # Carrega o vídeo
-    uploaded_file = st.file_uploader("Escolha um vídeo", type=["mp4"])
-    if uploaded_file is not None:
-        video = cv2.VideoCapture('https://vod-progressive.akamaized.net/exp=1683822149~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4428%2F15%2F397143934%2F1690405210.mp4~hmac=7e9d3739a29c9333e7d4184231f7c06e45094b985cf0589cb62b08d82a51078b/vimeo-prod-skyfire-std-us/01/4428/15/397143934/1690405210.mp4', cv2.CAP_ANY)
-        video_generator = detect_faces_video(video)
+    video_choice = {'video1': 'https://bit.ly/3pB4mmU',
+                    'video2': 'https://bit.ly/3LZSIZY',
+                    'video3': 'https://bit.ly/453LTzA',
+                    'video4': 'https://bit.ly/42MTkch'}
+    select_video = st.sidebar.selectbox('Selecione um vídeo', video_choice.keys())
+    video = cv2.VideoCapture(video_choice[select_video])
+    video_generator = detect_faces_video(video)
 
-        # Exibe o vídeo com as faces detectadas em tempo real
-        stframe = st.empty()
-        while True:
-            try:
-                frame = next(video_generator)
-                stframe.image(frame, channels="RGB")
-            except StopIteration:
-                break
+    # Exibe o vídeo com as faces detectadas em tempo real
+    stframe = st.empty()
+    while True:
+        try:
+            frame = next(video_generator)
+            stframe.image(frame, channels="RGB")
+        except StopIteration:
+            break
 
 
 if __name__=="__main__":
