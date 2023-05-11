89d55eb9aff9a3dfd92f9f57f210ee5d3e041d01
Hedvaldo Costa Vieira de Oliveira <hedvaldocostaV.O@hotmail.com>
2023-05-11 12:33:10 -0300

    V1

--- Main.py
+++ Main.py
@@ -1,26 +1,52 @@
-import streamlit as st
 import cv2
-from mtcnn import MTCNN
-import numpy as np
+import streamlit as st
 
 
-def detect_faces(image):
-    detector = MTCNN()
-    result = detector.detect_faces(image)
-    for face in result:
-        x, y, width, height = face['box']
-        cv2.rectangle(image, (x, y), (x+width, y+height), (0, 255, 0), 2)
-    return image
+def process_video(video_file):
+    # Carrega o vídeo
+    video_capture = cv2.VideoCapture(video_file.name)
+
+    # Define o modelo de detecção de rostos
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+    # Processa cada quadro do vídeo
+    while True:
+        # Lê um quadro do vídeo
+        ret, frame = video_capture.read()
+
+        # Verifica se a leitura do quadro foi bem sucedida
+        if not ret:
+            break
+
+        # Converte o quadro para escala de cinza para facilitar a detecção de rostos
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Detecta os rostos no quadro
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+        # Desenha um retângulo em volta de cada rosto detectado
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        # Mostra o quadro processado na janela de saída
+        cv2.imshow('Video', frame)
+
+        # Verifica se o usuário pressionou a tecla 'q' para sair
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Libera os recursos e fecha as janelas
+    video_capture.release()
+    cv2.destroyAllWindows()
 
 
-def main():
-    uploaded_file = st.sidebar.file_uploader("Carregue uma imagem", type=["jpg", "jpeg", "png"])
-    if uploaded_file is not None:
-        # decodificar uma imagem codificada em um formato específico em uma imagem OpenCV
-        image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
-        image = detect_faces(image)
-        st.image(image, channels="BGR")
+# Define o título do aplicativo
+st.title('Identificação de Rostos em Vídeo')
 
+# Adiciona um seletor de arquivos para permitir que o usuário carregue um vídeo
+video_file = st.file_uploader('Selecione um arquivo de vídeo', type=['mp4', 'avi'])
 
-if __name__ == "__main__":
-    main()
+if video_file is not None:
+    # Chama a função 'process_video' com o arquivo de vídeo selecionado
+    process_video(video_file)
+
