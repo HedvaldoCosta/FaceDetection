6a5f0a283871021ee2749eb36d082d9063544484
Hedvaldo Costa Vieira de Oliveira <hedvaldocostaV.O@hotmail.com>
2023-05-18 15:02:15 -0300

    Construção da classe

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+# Bibliotecas necessárias para a execução do código
 import streamlit as st
 import cv2
 from mtcnn import MTCNN
@@ -5,57 +6,56 @@ import numpy as np
 
 
 class FaceDetection:
-    def __init__(self, file_image=None, file_video=None):
+    def __init__(self, file_image='', file_video=''):
+        self.detector = MTCNN()
         self.image = file_image
         self.video = file_video
-        self.detector = MTCNN()
 
-    def detecting_faces_images(self):
-        result_img = self.detector.detect_faces(self.image)
-        for faces in result_img:
-            x, y, width, height = faces["box"]
-            cv2.rectangle(self.image, (x, y), (x + width, y + height), (0, 0, 255), 2)
+    def detecting_faces_image(self):
+        result = self.detector.detect_faces(self.image)
+        for faces in result:
+            x, y, width, height = faces['box']
+            cv2.rectangle(self.image, (x, y), (x+width, y+height), (0, 0, 255), 2)
         return self.image
 
-    def detecting_faces_videos(self):
+    def detecting_faces_video(self):
         while self.video.isOpened():
             ret, frame = self.video.read()
             if not ret:
                 break
-            result_video = self.detector.detect_faces(frame)
-            for faces in result_video:
-                x, y, width, height = faces["box"]
-                cv2.Rectangle(frame, (x, y), (x + width, y + height), (0, 0, 255), 2)
+            result = self.detector.detect_faces(frame)
+            for face in result:
+                x, y, w, h = face['box']
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            # Usado para iterar sobre uma sequência de valores.
             yield frame
 
+    def detecting_faces_webcam(self):
+        cap = cv2.VideoCapture(0)
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
 
-if __name__=="__main__":
-    st.title("Detector de faces com MTCNN")
-    video_choice = {
-        '': '',
-        'video1': 'https://bit.ly/3pB4mmU',
-        'video2': 'https://bit.ly/3LZSIZY',
-        'video3': 'https://bit.ly/453LTzA',
-        'video4': 'https://bit.ly/42MTkch'
-    }
-    select_video = st.sidebar.selectbox('Selecione um vídeo', video_choice.keys())
-    video = cv2.VideoCapture(video_choice[select_video])
-    face_detection_video = FaceDetection(file_video=video)
-    video_generator = face_detection_video.detecting_faces_videos()
+            # Detecção de rosto usando MTCNN
+            results = self.detector.detect_faces(frame)
 
-    stframe = st.empty()
-    while True:
-        try:
-            frame = next(video_generator)
-            stframe.image(frame, channels="RGB")
-        except StopIteration:
-            break
+            # Desenhar retângulos em torno dos rostos detectados
+            for result in results:
+                x, y, w, h = result['box']
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    uploaded_file_img = st.sidebar.file_uploader("Carregue uma imagem", type=["jpg", "jpeg", "png"])
-    if uploaded_file_img is not None:
-        image = cv2.imdecode(np.fromstring(uploaded_file_img.read(), np.uint8), 1)
-        face_detection_img = FaceDetection(file_image=image)
-        image = face_detection_img.detecting_faces_images()
-        st.image(image, channels="BGR")
+            # Mostrar o quadro com os retângulos dos rostos detectados
+            cv2.imshow('Webcam', frame)
+
+            # Condição de saída do loop
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        # Libere os recursos
+        cap.release()
+        cv2.destroyAllWindows()
+
+
+
+
